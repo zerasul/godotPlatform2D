b2e7b1e374fcf9b0b7e3cb9255630eeb5c45147c
Victor <zerasul@gmail.com>
2025-03-11 21:15:18 +0100

    initial Commit

---
+++ enemy.gd
@@ -0,0 +1,46 @@
+extends CharacterBody2D
+
+const WALK_SPEED = 1000
+
+const LEFT= -1
+const RIGHT = 1
+const STOP = 0
+
+const DIRECTIONS = [LEFT,STOP,RIGHT]
+
+@onready var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var currentDir = 0
+var direction = LEFT
+
+func _ready() -> void:
+	$directiontimer.start()
+	$AnimatedSprite2D.play("move")
+	
+func _physics_process(delta: float) -> void:
+	var walk = WALK_SPEED* direction
+	
+	velocity.x = walk * delta
+
+	# Vertical movement code. Apply gravity.
+	velocity.y += gravity * delta
+	
+	move_and_slide()
+	
+
+	
+
+
+func _on_directiontimer_timeout() -> void:
+	currentDir+=1
+	direction= DIRECTIONS[currentDir % DIRECTIONS.size()]
+	if direction!=0 :
+		$AnimatedSprite2D.play("move")
+	else:
+		$AnimatedSprite2D.play("idle")
+		
+	if(direction<0):
+		$AnimatedSprite2D.flip_h=false
+	else:
+		$AnimatedSprite2D.flip_h=true
+	pass # Replace with function body.
